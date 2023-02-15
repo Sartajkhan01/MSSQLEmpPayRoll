a5840d42fe2d9ef463a06462b41ba876770f64c9
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-02-15 18:49:09 +0530

    [Sartaj] : added inserting details for empolyee payroll

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,3 +10,11 @@ Id int identity (1,1) primary key,
 Name varchar(200),
 Salary float,
 StartDate date);
+
+--UC3-- Create employee_payroll data as part of CURD Operation
+insert into employee_payroll (Name, Salary, StartDate) values
+('Dhoni', 70000.00, '2007-02-03'),
+('Virat', 50000.00, '2010-05-04'),
+('Rohit', 60000.00, '2009-06-09'),
+('Smrithi', 40000.00, '2018-03-05'),
+('Mithai', 50000.00, '2020-08-02');
