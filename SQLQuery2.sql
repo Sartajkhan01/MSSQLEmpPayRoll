167e10ad734e0e587da70513bd4fbc3c5b0052cf
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-02-15 18:47:00 +0530

    [Sartaj] : added creating table for empolyee payroll

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -3,3 +3,10 @@
 --UC1-- Create Payroll_Service Database
 create database Payroll_Service;
 use Payroll_Service;
+
+--UC2-- employee_payroll table
+Create Table employee_payroll(
+Id int identity (1,1) primary key,
+Name varchar(200),
+Salary float,
+StartDate date);
